a0b08e6aca9c13c48f5a5a4207d19f8b3c1b35ba
Satyam-Private <satyampatil1252@gmail.com>
2024-12-15 22:58:01 +0530

    did revision and rewrite this code

--- knapsack.c
+++ knapsack.c
@@ -2,34 +2,42 @@
 #include<conio.h>
 #include<math.h>
 int t[100][1000];
-int knapsack(int wt[] , int val[] , int w , int n){
-    if(n == 0 || w == 0){
+int knapsack(int val[] , int wt[] , int W , int n){
+     if(W == 0 || n == 0){
         return 0;
     }
-    if(t[n][w] != -1){
-        return t[n][w];
-    }
-    if(wt[n-1] <= w){
-        return t[n][w] = fmax(val[n-1] + knapsack(wt , val , w - wt[n-1] , n -1),knapsack(wt  ,val , w , n -1));
-
-    }
-    else if (wt[n - 1]  > w)
+    if (t[n][W] != -1)
     {
-        return t[n][w] =knapsack(wt , val , w  , n-1);
+        return t[n][W];
+    }
+    
+   
+    if(wt[n-1] <= W){
+        return t[n][W] = fmax(val[n-1] + knapsack(val , wt , W - wt[n-1], n-1) , knapsack(val , wt , W , n-1) );
+    }
+    else if (wt[n-1] > W)
+    {
+        return t[n][W] = knapsack(val , wt , W , n -1);
     }
     
 }
 
 int main(){
-    int wt[] = {1 , 3 , 4 ,5};
-    int val[] = {1 , 4, 5 , 7};
-    int w = 7;
-    int n = sizeof(wt) / sizeof(wt[0]);
-    for(int i = 0; i <= n; i++){
-        for(int j = 0; j <= w; j++){
+    int val[] = {1, 2, 3, 5};
+    int wt[] = {2, 7, 4, 1};
+    int W = 7; 
+    int n = sizeof(val)/sizeof(val[0]);
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <=W+1; j++)
+        {
             t[i][j] = -1;
         }
+        
     }
-    int profit = knapsack(wt , val , w , n);
-    printf("the profit is %d " , profit);
+    
+    int MaxProfit = knapsack(val , wt , W , n);
+    printf("the maximum profit is %d " , MaxProfit);
+
+    
 }
